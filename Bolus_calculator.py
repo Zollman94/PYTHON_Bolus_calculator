51a70653964769559711b0e46e7fb14e543508cd
Filip Zollman <140598268+Zollman94@users.noreply.github.com>
2023-12-16 13:52:39 +0100

    Update Bolus_calculator.py
    English translation

--- Bolus_calculator.py
+++ Bolus_calculator.py
@@ -2,25 +2,27 @@ import tkinter as tk
 import csv
 import datetime
 
-# global variables
+# Global variables
 
-#input values
+# Input values
 carbs = float(0)
 fats = float(0)
 proteins = float(0)
 grams = float(0)
-#return values
+
+# Return values
 boluscarbs = float(0)
 bolusfats = float(0)
 bolustotal = float(0)
-#constant values
+
+# Constant values
 SENSITIVITY = float(12)
 
-#Define exit program
+# Define exit program
 def exit_program():
     root.destroy()
 
-#Saves all given values
+# Save all given values
 def save_values():
     global carbs, fats, proteins, grams, bolustotal, bolusfats, boluscarbs
     carbs = float(entry_carbs.get())
@@ -29,14 +31,14 @@ def save_values():
     grams = float(entry_grams.get())
     calculate()
 
-#Updates return text
+# Update return text
 def update_display_value():
     global carbs, fats, proteins, grams, bolustotal, bolusfats, boluscarbs
     display_value1.config(text=f"Total bolus: {bolustotal}")
-    display_value2.config(text=f"Za sacharidy: {boluscarbs}")
-    display_value3.config(text=f"Za tuky: {bolusfats}")
+    display_value2.config(text=f"Bolus for carbs: {boluscarbs}")
+    display_value3.config(text=f"Bolus for fats: {bolusfats}")
 
-#Calculates values and updates return text
+# Calculate values and update return text
 def calculate():
     global carbs, fats, proteins, grams, bolustotal, bolusfats, boluscarbs
     boluscarbs = ((carbs / SENSITIVITY) / 100) * grams
@@ -47,7 +49,7 @@ def calculate():
     bolustotal = round(bolustotal, 1)
     update_display_value()
 
-#Logs and resets entry values to prevent duplicate presses
+# Log and reset entry values to prevent duplicate presses
 def log():
     global carbs, fats, proteins, grams, bolustotal, bolusfats, boluscarbs
     if (bolustotal == 0):
@@ -64,7 +66,7 @@ def log():
             writer.writerow([time, carbs, fats, proteins, bolustotal])
         reset()
 
-#Reset everything
+# Reset everything
 def reset():
     global carbs, fats, proteins, grams, bolustotal, bolusfats, boluscarbs
     carbs = float(0)
@@ -79,57 +81,55 @@ def reset():
     entry_proteins.delete(0, tk.END)
     entry_fats.delete(0, tk.END)
     entry_grams.delete(0, tk.END)
-    
-#main window
+
+# Main window
 root = tk.Tk()
 root.title("Bolus Calculator")
 root.geometry("350x200")
 
+# Entry fields
 
-#ENTRY FIELDS#
-
-#carbs
-label_carbs = tk.Label(root, text="Sacharidy:")
+# Carbs
+label_carbs = tk.Label(root, text="Carbs:")
 label_carbs.place(x=0, y=50)
 entry_carbs = tk.Entry(root)
 entry_carbs.place(x=60, y=50)
 entry_carbs.bind("<Return>", lambda event, :save_values())
 
-#fats
-label_fats = tk.Label(root, text="Tuky:")
+# Fats
+label_fats = tk.Label(root, text="Fats:")
 label_fats.place(x=0, y=80)
 entry_fats = tk.Entry(root)
 entry_fats.place(x=60, y=80)
 entry_fats.bind("<Return>", lambda event, :save_values())
 
-#proteins
-label_proteins = tk.Label(root, text="Bílkoviny:")
+# Proteins
+label_proteins = tk.Label(root, text="Proteins:")
 label_proteins.place(x=0, y=110)
 entry_proteins = tk.Entry(root)
 entry_proteins.place(x=60, y=110)
 entry_proteins.bind("<Return>", lambda event, :save_values())
 
-#grams
-label_grams = tk.Label(root, text="Gramy:")
+# Grams
+label_grams = tk.Label(root, text="Grams:")
 label_grams.place(x=0, y=140)
 entry_grams = tk.Entry(root)
 entry_grams.place(x=60, y=140)
 entry_grams.bind("<Return>", lambda event, :save_values())
 
-
-#TEXT#
+# Text
 
 #label title
-label = tk.Label(root, text="Kalkulačka inzulinu")
+label = tk.Label(root, text="Bolus calculator")
 label.place(x=50, y=20)
 #label total bolus
 display_value1 = tk.Label(root, text="Total bolus: ")
 display_value1.place(x=200, y=20)
-#label bolus za sacharidy
-display_value2 = tk.Label(root, text="Bolus za sacharidy: ")
+#label bolus for carbs
+display_value2 = tk.Label(root, text="Bolus for carbs: ")
 display_value2.place(x=200, y=40)
-#label bolus za tuky
-display_value3 = tk.Label(root, text="Bolus za tuky: ")
+#label bolus for fats
+display_value3 = tk.Label(root, text="Bolus for fats:")
 display_value3.place(x=200, y=60)
 
 
@@ -142,7 +142,7 @@ exit_button.place(x=200, y=120)
 calculate_button = tk.Button(root, text="Calculate", command=save_values)
 calculate_button.place(x=200, y=90)
 #log button
-calculate_button = tk.Button(root, text="Logovat", command=log)
+calculate_button = tk.Button(root, text="Log", command=log)
 calculate_button.place(x=260, y=90)
 
 root.mainloop()
